0d1b40dcfb8f652037bcd431cfac366f384abede
Sergey Vershinin <sergey@convermax.com>
2020-11-25 16:02:23 +0300

    Create convermax-dev.user.js

---
+++ convermax-dev.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         convermax-dev
+// @namespace    convermax-dev
+// @version      1
+// @run-at       document-start
+// @grant        none
+// @include      http://*
+// @include      https://*
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  window.Convermax = { loaded: true };
+
+  new MutationObserver((_, observer) => {
+    const scriptTag = document.querySelector('script[src*="convermax.com"]');
+
+    if (scriptTag) {
+      scriptTag.src = '';
+      scriptTag.remove();
+
+      setTimeout(() => {
+        window.Convermax = {};
+
+        injectScript('https://localhost:3000/vendor.dev.bundle.js');
+        injectScript('https://localhost:3000/templates.js');
+        injectScript('https://localhost:3000/main.js');
+      }, 500);
+
+      observer.disconnect();
+    }
+  })
+    .observe(document.documentElement, { childList: true, subtree: true });
+
+  function injectScript(src) {
+    const scriptTag = document.createElement('script');
+    scriptTag.src = src;
+    scriptTag.async = false;
+    document.body.appendChild(scriptTag);
+  }
+
+})();
