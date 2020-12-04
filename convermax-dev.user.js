4792d94e8327c19d17abbc8f3c33e2866a5cdf5f
Dmitry Shpak <jazzmanpw@mail.ru>
2020-12-04 16:53:24 +0300

    Support multiple scripts and preinjected styles

--- convermax-dev.user.js
+++ convermax-dev.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         convermax-dev
 // @namespace    convermax-dev
-// @version      7
+// @version      8
 // @run-at       document-start
 // @grant        none
 // @include      http://*
@@ -10,32 +10,41 @@
 
 (function() {
   'use strict';
-  
+
   window.Convermax = window.Convermax || {};
   window.Convermax.loaded = true;
 
   new MutationObserver((_, observer) => {
-    const scriptTag = document.querySelector('script[src*="convermax.com"]');
+    const scriptTag = [...document.querySelectorAll('script[src*="convermax.com"]')].find(s => s.src.includes('search.min.js'));
 
     if ((scriptTag || localStorage["cm_inject-script"]) && !window.ConvermaxInjected) {
       if (scriptTag) {
         scriptTag.src = '';
-        scriptTag.remove();        
+        scriptTag.remove();
       }
 
       window.ConvermaxInjected = true;
-      
+
       setTimeout(() => {
+        observer.disconnect();
+
         window.Convermax.loaded = false;
 
         console.log('%cConvermax DEV UserScript', 'color: palevioletred; background: darkslateblue; font-size: 44px;')
-        
+
         injectScript('https://localhost:3000/vendor.dev.bundle.js');
         injectScript('https://localhost:3000/templates.js');
         injectScript('https://localhost:3000/main.js');
       }, 500);
+    }
 
-      observer.disconnect();
+    const styleTag = document.querySelector('link[href*="convermax.com"]');
+
+    if (styleTag) {
+      styleTag?.remove();
+      const localStyleTag = document.createElement('link');
+      localStyleTag.rel = 'stylesheet';
+      localStyleTag.href = 'https://localhost:3000/search.css';
     }
   })
     .observe(document.documentElement, { childList: true, subtree: true });
