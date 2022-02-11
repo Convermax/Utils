56a81a685306a2a8faf9ebefc78aea1d3ab6178b
Andrey Grebenyk <35263631+x318@users.noreply.github.com>
2022-02-11 20:32:41 +0300

    repair mutationObserver & styles hot reload

--- convermax-dev.user.js
+++ convermax-dev.user.js
@@ -1,61 +1,84 @@
 // ==UserScript==
 // @name         convermax-dev
 // @namespace    convermax-dev
-// @version      14
+// @version      15
 // @run-at       document-start
 // @grant        none
 // @match        *://*/*
 // ==/UserScript==
 
-(function() {
-  'use strict';
+;(function () {
+  'use strict'
 
-  window.Convermax = window.Convermax || {};
-  window.Convermax.loaded = true; // for the legacy script version
-  window.Convermax.devScriptEnabled = true;
+  window.Convermax = window.Convermax || {}
+  window.Convermax.loaded = true // for the legacy script version
+  window.Convermax.devScriptEnabled = true
 
-  document.addEventListener("DOMContentLoaded", function() {
+  function createMutatuinObserver() {
+    if (!document.documentElement) {
+      window.setTimeout(createMutatuinObserver, 200)
+      return
+    }
+
     new MutationObserver((_, observer) => {
-    const scriptTag = [...document.querySelectorAll('script[src*="convermax.com"]')].find(s => s.src.includes('search.min.js'));
+      const scriptTag = [...document.querySelectorAll('script[src*="convermax.com"]')].find((s) =>
+        s.src.includes('search.min.js')
+      )
 
-    if ((scriptTag || localStorage["cm_inject-script"]) && !window.ConvermaxDevScriptInjected) {
-      if (scriptTag) {
-        scriptTag.src = '';
-        scriptTag.remove();
+      if ((scriptTag || localStorage['cm_inject-script']) && !window.ConvermaxDevScriptInjected) {
+        if (scriptTag) {
+          scriptTag.src = ''
+          scriptTag.remove()
+        }
+
+        window.ConvermaxDevScriptInjected = true
+
+        setTimeout(() => {
+          observer.disconnect()
+
+          window.Convermax.loaded = false // for the legacy script version
+
+          console.log(
+            '%cConvermax DEV UserScript',
+            'color: white; background: black; font-size: 44px; text-align: center; padding: 1rem; text-transform: uppercase; font-family: -apple-system, BlinkMacSystemFont, Roboto, Oxygen, , Cantarell, sans-serif;'
+          )
+
+          injectScript('https://localhost:3000/vendor.dev.bundle.js')
+          injectScript('https://localhost:3000/templates.js')
+          injectScript('https://localhost:3000/main.js')
+        }, 1000)
       }
 
-      window.ConvermaxDevScriptInjected = true;
+      const styleTag = document.querySelector('link[href*="convermax.com"]')
 
-      setTimeout(() => {
-        observer.disconnect();
-
-        window.Convermax.loaded = false; // for the legacy script version
-
-        console.log('%cConvermax DEV UserScript', 'color: palevioletred; background: darkslateblue; font-size: 44px;')
-
-        injectScript('https://localhost:3000/vendor.dev.bundle.js');
-        injectScript('https://localhost:3000/templates.js');
-        injectScript('https://localhost:3000/main.js');
-      }, 500);
-    }
-
-    const styleTag = document.querySelector('link[href*="convermax.com"]');
-
-    if (styleTag) {
-      const localStyleTag = document.createElement('link');
-      localStyleTag.rel = 'stylesheet';
-      localStyleTag.href = 'https://localhost:3000/search.css';
-      styleTag.parentElement.replaceChild(localStyleTag, styleTag);
-    }
-  })
-    .observe(document.documentElement, { childList: true, subtree: true });
-});
-
-  function injectScript(src) {
-    const scriptTag = document.createElement('script');
-    scriptTag.src = src;
-    scriptTag.async = false;
-    document.body.appendChild(scriptTag);
+      if (styleTag) {
+        const localStyleTag = document.createElement('link')
+        localStyleTag.rel = 'stylesheet'
+        localStyleTag.href = 'https://localhost:3000/search.css'
+        styleTag.parentElement.replaceChild(localStyleTag, styleTag)
+      }
+    }).observe(document.documentElement, { childList: true, subtree: true })
   }
 
-})();
+  createMutatuinObserver()
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'F9') {
+      reloadCss()
+    }
+  })
+
+  function injectScript(src) {
+    const scriptTag = document.createElement('script')
+    scriptTag.src = src
+    scriptTag.async = false
+    document.body.appendChild(scriptTag)
+  }
+
+  function reloadCss() {
+    var links = document.getElementsByTagName('link')
+    for (var cl in links) {
+      var link = links[cl]
+      if (link.rel === 'stylesheet' && link.href === 'https://localhost:3000/search.css') link.href += ''
+    }
+  }
+})()
