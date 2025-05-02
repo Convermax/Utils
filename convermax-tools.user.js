5349783c2d110c8deceaa166c0ebfb9b9e8acc07
Mikhail Savelev <mikhail@convermax.com>
2025-05-02 16:55:52 +0300

    [Tools] Use the generic way to detect Shopify stub ref https://motoscoot-test.myshopify.com/password https://asfir-4x4.myshopify.com/password https://ultrapowermarket.com/password

--- convermax-tools.user.js
+++ convermax-tools.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Convermax Tools
 // @namespace    convermax-dev
-// @version      0.9.2
+// @version      0.9.3
 // @description  Convermax Tools
 // @downloadURL  https://github.com/Convermax/Utils/raw/main/convermax-tools.user.js
 // @updateURL    https://github.com/Convermax/Utils/raw/main/convermax-tools.user.js
@@ -529,10 +529,7 @@ function fixNoStoreAtShopifyPartners() {
 }
 
 function bypassShopifyStub() {
-  if (
-    window.location.pathname === '/password' &&
-    window.document.querySelector('#shopify-section-main-password-footer')
-  ) {
+  if (window.location.pathname === '/password' && actions.platforms.shopify.test()) {
     window.location.assign(`${window.location.origin}/admin/themes`);
   }
 }
