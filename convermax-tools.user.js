c0b56f71580c05fdaa5499e363f3fea229a68d3b
Mikhail Savelev <mikhail@convermax.com>
2025-08-22 19:58:25 +0300

    Bump version

--- convermax-tools.user.js
+++ convermax-tools.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Convermax Tools
 // @namespace    convermax-dev
-// @version      0.9.9
+// @version      0.9.10
 // @description  Convermax Tools
 // @downloadURL  https://github.com/Convermax/Utils/raw/main/convermax-tools.user.js
 // @updateURL    https://github.com/Convermax/Utils/raw/main/convermax-tools.user.js
